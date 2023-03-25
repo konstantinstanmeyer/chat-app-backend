b9d23a2702b4c7f2cb28c0261fea4a70c87a248d
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-03-25 13:54:40 -0600

    basic socket setup

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,19 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("yes");
+const express = require('express');
+const { Server } = require("socket.io");
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+});
+io.on('connection', (socket) => {
+    socket.on('message', (value) => {
+        socket.broadcast.emit('incoming-message', value);
+    });
+});
+server.listen(3001, () => {
+    console.log('Listening on port 3001');
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("yes")
+const express = require('express');
+const { Server } = require("socket.io");
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+})
+
+io.on('connection', (socket) =>  {
+    socket.on('message', (value) => {
+        socket.broadcast.emit('incoming-message', value);
+    })
+})
+
+server.listen(3001, () => {
+    console.log('Listening on port 3001')
+})
